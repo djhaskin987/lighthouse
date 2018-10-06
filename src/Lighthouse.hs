fbc395be03df6ef3d313f9b02e31a01f2da5e58b
Daniel Jay Haskin <djhaskin987@gmail.com>
2018-10-06 08:46:42 -0600

    Whitespace

--- src/Lighthouse.hs
+++ src/Lighthouse.hs
@@ -2,16 +2,14 @@ module Lighthouse (Node (Node), Workload (Workload), assignWorkload) where
 
 import qualified Data.Map as Map
 
-data Node = Node {
-    nodeId :: String
-  , resources :: Map.Map String Float
-  , workloads :: [Workload]
-} deriving (Show, Eq)
+data Node = Node { nodeId :: String
+                 , resources :: Map.Map String Float
+                 , workloads :: [Workload]
+                 } deriving (Show, Eq)
 
-data Workload = Workload {
-    loadId :: String
-  , requirements :: Map.Map String Float
-} deriving (Show, Eq)
+data Workload = Workload { loadId :: String
+                         , requirements :: Map.Map String Float
+                         } deriving (Show, Eq)
 
 assignWorkload :: [Node] -> Workload -> Maybe [Node]
 assignWorkload nodes load = Just nodes
