185e492f18edfc9fcc7fd1b6b78ebe8d0932cbe8
Daniel Jay Haskin <djhaskin987@gmail.com>
2018-10-17 18:29:31 -0600

    Adding more tests

--- src/Lighthouse.hs
+++ src/Lighthouse.hs
@@ -1,6 +1,7 @@
-module Lighthouse (Node (Node), Workload (Workload), assignWorkload) where
+module Lighthouse (Node (Node), Workload (Workload), assignWorkload, place) where
 
 import qualified Data.Map as Map
+import qualified Control.Monad as Monad
 
 data Node = Node { nodeId :: String
                  , resources :: Map.Map String Float
@@ -11,6 +12,62 @@ data Workload = Workload { loadId :: String
                          , requirements :: Map.Map String Float
                          } deriving (Show, Eq)
 
+-- Replace the first element of the list for which the computation `(a -> Maybe
+-- a)` was successful with its result, or return Nothing if no computation
+-- worked for the whole list.
+place :: (a -> Maybe a) -> [a] -> Maybe [a]
+place f [] = Nothing
+place f (x:xs) = case f x of
+                 Nothing -> case place f xs of
+                              Nothing -> Nothing
+                              Just ys -> Just (x:ys)
+                 Just y -> Just (y:xs)
+
+attachWorkload :: Workload -> Node -> Maybe Node
+attachWorkload load (Node id have loads)
+  | Map.size used < Map.size need = Nothing
+  | not isAllPositive = Nothing
+  | otherwise =
+    Just (Node
+      id
+      leftovers
+      (load : loads))
+  where
+    -- Subtract what is required from what is available, and only show the
+    -- difference for the keys that exist in both
+    used = Map.intersectionWith
+                  (-)
+                  have
+                  need
+    need = requirements load
+    -- Check to make sure all that there were in fact enough resources
+    -- to meet the requirements' demands
+    isAllPositive = Map.foldr
+      (\v c -> v >= 0 && c)
+      True
+      used
+    -- Update values in `have` with values in `used`
+    leftovers = Map.unionWith
+      (flip const)
+      have
+      used
+
 assignWorkload :: [Node] -> Workload -> Maybe [Node]
-assignWorkload [] load = Just []
-assignWorkload nodes load = Just nodes
+assignWorkload nodes load =
+  place (attachWorkload load) nodes
+
+sortNodes :: [Node] -> [Node]
+sortNodes nodes = nodes
+
+sortWorkloads :: [Workload] -> [Workload]
+sortWorkloads workloads = workloads
+
+assignWorkloads :: [Node] -> [Workload] -> Maybe [Node]
+assignWorkloads nodes loads = Monad.foldM assignWorkload sortedNodes sortedLoads
+  where
+    sortedNodes = sortNodes nodes
+    sortedLoads = sortWorkloads loads
+
+-- The above is a lot to bite off, so we'll work on going from a list of nodes
+-- and their workloads to a list of workloads and their nodes using ix-set
+-- another time.
--- tests/Tests.hs
+++ tests/Tests.hs
@@ -4,29 +4,41 @@ import qualified Data.Map as Map
 import Lighthouse
 import Test.Hspec
 
-main :: IO ()
+testPlace =
+    describe "empty place" $
+      it "will find 5 and place it" $
+        Lighthouse.place a [1,2,3,4,5] `shouldBe` Just [1,2,3,4,88]
+  where
+    a 5 = Just 88
+    a x = Nothing
 
-main = hspec $ do
+testAssignWorkload = do
     describe "empty nodes" $
         it "is supposed to return empty list" $
-            (Lighthouse.assignWorkload [] req) `shouldBe` Just []
+            Lighthouse.assignWorkload [] req `shouldBe` Nothing
     describe "standard case" $
         it "is supposed to register new workloads" $
             Lighthouse.assignWorkload nodes req `shouldBe` returned
   where
-      firstNode = Lighthouse.Node "first"
-                                  (Map.fromList [("cpu", 4.0), ("mem", 8.0)])
-                                  []
-      secondNode = Lighthouse.Node
-        "second"
-        (Map.fromList [("cpu", 2.0), ("mem", 4.0)])
-        []
-      firstNodeModified = Lighthouse.Node
-        "first"
-        (Map.fromList [("cpu", 0.2), ("mem", 5.6)])
-        [req]
-      nodes = [firstNode, secondNode]
-      returned = Just [firstNodeModified, secondNode]
-      req = Lighthouse.Workload
-        "myleia"
-        (Map.fromList [("cpu", 3.8), ("mem", 2.4)])
+    firstNode = Lighthouse.Node
+      "first"
+      (Map.fromList [("cpu", 4.0), ("mem", 8.0)])
+      []
+    secondNode = Lighthouse.Node
+      "second"
+      (Map.fromList [("cpu", 2.0), ("mem", 4.0)])
+      []
+    firstNodeModified = Lighthouse.Node
+      "first"
+      (Map.fromList [("cpu", 0.2), ("mem", 5.6)])
+      [req]
+    nodes = [firstNode, secondNode]
+    returned = Just [firstNodeModified, secondNode]
+    req = Lighthouse.Workload
+      "myleia"
+      (Map.fromList [("cpu", 3.8), ("mem", 2.4)])
+
+main :: IO ()
+main = hspec $ do
+  testAssignWorkload
+  testPlace
